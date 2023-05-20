a948eb1c50333d78993e52d8f7b49fcadee03b7a
jacobaranza <jacobthomasjunior@gmail.com>
2023-05-20 09:57:13 -0500

    mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('#create-task-form');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+  
+      const input = document.querySelector('#new-task-description');
+      const task = input.value;
+  
+      const taskList = document.querySelector('#tasks');
+      const newTask = document.createElement('li');
+      newTask.textContent = task;
+      taskList.appendChild(newTask);
+  
+      input.value = '';
+    });
+  });
+  
+
